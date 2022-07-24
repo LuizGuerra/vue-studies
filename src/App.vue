cc4261d149ccd453bc194ecf917cc13cea679f89
Luiz Guerra <luizpedrofg@gmail.com>
2022-07-24 18:17:34 -0300

    todo

---
+++ src/App.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="container">
+    <Header title="Task Tracker" />
+    <Tasks :tasks="tasks" />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Tasks
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Doctor',
+        day: 'March 1st',
+        reminder: true
+      },
+      {
+        id: 2,
+        text: 'School',
+        day: 'March 3rd',
+        reminder: true
+      },
+      {
+        id: 3,
+        text: 'Market',
+        day: 'March 3rd',
+        reminder: false
+      },
+    ]
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+
+.btn {
+  display: inline-block;
+  background: black;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 20px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.btn:active {
+  transform: scale(0.98);
+}
+
+.btn-block {
+  display: block;
+  width: 100%;
+}
+
+</style>
